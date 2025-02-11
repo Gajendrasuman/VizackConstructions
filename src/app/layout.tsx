5b1307e0bfd6419f421cc646cbb8626b0b7cbe74
Farhan Khan <farhankhan.code@gmail.com>
2025-02-11 17:16:58 +0000

    add loader and minor bug fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,28 @@
+"use client"
+import  {metadata}  from "./metadata"
 import "@/styles/globals.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { meta } from "@/metadata/meta"
-export const metadata = meta
+import { useState, useEffect } from "react"
+import { usePathname } from "next/navigation"
+import Loader from "./components/Loader"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setLoading(true) 
+    const timer = setTimeout(() => setLoading(false), 500)
+    return () => clearTimeout(timer)
+  }, [pathname])
+
   return (
     <html lang="en">
-      <body className={`bg-[#EBE7E1]`}>
+      <body className="bg-[#EBE7E1]">
+        {loading && <Loader />}
         <Header />
         <main>{children}</main>
         <Footer />
@@ -19,4 +30,3 @@ export default function RootLayout({
     </html>
   )
 }
-
